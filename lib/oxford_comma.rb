204d5772e02ad94a31b06a64201383f9d12d642d
Michael Fuget <michaelfuget@gmail.com>
2020-12-01 07:43:00 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,7 @@
 def oxford_comma(array)
   last_element = array.last
   array.pop
-  array.join(", ")
-  array.push(", and #{last_element}")
+  out = array.join(", ")
+  out.push(", and #{last_element}")
   return array
 end
