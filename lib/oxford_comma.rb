89b33a64f4e18589977b9b747c8d5359846a0394
Michael Fuget <michaelfuget@gmail.com>
2020-12-01 07:46:10 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  last_element = array.last
+  array.pop
+  out = array.join(", ")
+  if(array.size ==1)
+    out <<(" and #{last_element}")
+  elsif(array.size ==0)
+    out <<("#{last_element}")
+  else
+    out <<(", and #{last_element}")
+  end
+  return out
 end
